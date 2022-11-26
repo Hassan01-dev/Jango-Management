cb0b20c1a0c62d5a83e2ad88bfd35066f1d1e9b5
zunair <zunair.dev@gmail.com>
2022-11-27 02:00:34 +0500

    updated enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :invitable
 
-  enum designation: [ :user, :admin, :super_admin ]
+  enum designation: { user: 0, admin: 1, super_admin: 2 }
 end
